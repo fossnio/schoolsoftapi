a6bc73004b03c6928cfaabc548731229b2f3716e
William Wu <william@pylabs.org>
2017-09-13 09:16:54 +0800

    因應校務系統強制走 https，並避開因非法 certs 造成之連線錯誤

--- schoolsoftapi/schoolsoftapi.py
+++ schoolsoftapi/schoolsoftapi.py
@@ -11,7 +11,7 @@ import requests
 class SchoolSoftAPI:
     '''透過 WEBUI 介接全誼校務系統'''
 
-    def __init__(self, username, password, semester, baseurl='http://eschool.tp.edu.tw'):
+    def __init__(self, username, password, semester, baseurl='https://eschool.tp.edu.tw'):
         '''初始化'''
 
         self.username = username
@@ -25,12 +25,12 @@ class SchoolSoftAPI:
     def login(self):
         '''登入校務系統'''
 
-        self.session.get('{0}/index.jsp'.format(self.baseurl))
-        self.session.post('{0}/login.jsp'.format(self.baseurl), data={'method': 'getLogin', 'auth_type': '', 'auth_role': '', 'showTitle': '0'})
+        self.session.get('{0}/index.jsp'.format(self.baseurl), verify=False)
+        self.session.post('{0}/login.jsp'.format(self.baseurl), data={'method': 'getLogin', 'auth_type': '', 'auth_role': '', 'showTitle': '0'}, verify=False)
 
         # 重複跑直到 capcha 成功辨識出是 5 個數字（對錯不管）
         while True:
-            self.response = self.session.get('{0}/web-sso/rest/Redirect/login/page/normal?returnUrl={0}/WebAuth.do'.format(self.baseurl))
+            self.response = self.session.get('{0}/web-sso/rest/Redirect/login/page/normal?returnUrl={0}/WebAuth.do'.format(self.baseurl), verify=False)
 
             # 取得 post 網址
             post_url = re.findall(r' action="(.+?)"', self.response.text)[0]
@@ -38,7 +38,7 @@ class SchoolSoftAPI:
             # 圖形認證碼下載並丟給 tesseract 直到辨認出是 5 個數字
             while True:
 
-                self.response = self.session.get('{0}/RandomNum?t={1}'.format(self.baseurl, int(datetime.now().timestamp() * 1000)), stream=True)
+                self.response = self.session.get('{0}/RandomNum?t={1}'.format(self.baseurl, int(datetime.now().timestamp() * 1000)), stream=True, verify=False)
 
                 with open('/tmp/random_number.jpeg', 'wb') as f:
                     shutil.copyfileobj(self.response.raw, f)
@@ -53,7 +53,7 @@ class SchoolSoftAPI:
                     time.sleep(5)
 
             # 認證
-            self.response = self.session.post('{0}{1}'.format(self.baseurl, post_url), data={'username': self.username, 'password': self.password, 'random_num': captcha_number})
+            self.response = self.session.post('{0}{1}'.format(self.baseurl, post_url), data={'username': self.username, 'password': self.password, 'random_num': captcha_number}, verify=False)
 
             if '登入失敗' in self.response.text:
                 # 等 5 分鐘，避開失敗 5 次被停權 15 分鐘的限制
