15ed5e6fbc48eb05f240c0a8fc7a45f94595bd52
William Wu <william@pylabs.org>
2017-09-14 01:33:26 +0800

    登入可指定重試次數與等待時間

--- schoolsoftapi/schoolsoftapi.py
+++ schoolsoftapi/schoolsoftapi.py
@@ -24,14 +24,14 @@ class SchoolSoftAPI:
         self.session.headers.update({'User-Agent': '"Mozilla/5.0 (X11; Linux x86_64; rv:55.0) Gecko/20100101 Firefox/55.0"'})
         self.response = None
 
-    def login(self):
+    def login(self, retry=True, wait=300):
         '''登入校務系統'''
 
         self.session.get('{0}/index.jsp'.format(self.baseurl), verify=False)
         self.session.post('{0}/login.jsp'.format(self.baseurl), data={'method': 'getLogin', 'auth_type': '', 'auth_role': '', 'showTitle': '0'}, verify=False)
 
         # 重複跑直到 capcha 成功辨識出是 5 個數字（對錯不管）
-        while True:
+        while retry:
             self.response = self.session.get('{0}/web-sso/rest/Redirect/login/page/normal?returnUrl={0}/WebAuth.do'.format(self.baseurl), verify=False)
 
             # 取得 post 網址
@@ -53,16 +53,21 @@ class SchoolSoftAPI:
                 if captcha_number and captcha_number.isdigit() and len(captcha_number) == 5:
                     break
                 else:
-                    time.sleep(5)
+                    time.sleep(1)
 
             # 認證
             self.response = self.session.post('{0}{1}'.format(self.baseurl, post_url), data={'username': self.username, 'password': self.password, 'random_num': captcha_number}, verify=False)
 
             if '登入失敗' in self.response.text:
-                # 等 5 分鐘，避開失敗 5 次被停權 15 分鐘的限制
-                time.sleep(300)
+                # 等待，避開失敗 5 次被停權 15 分鐘的限制
+                time.sleep(wait)
             else:
-                break
+                return True
+
+            if retry is not True:
+                retry -= 1
+                if retry == 0:
+                    return False
 
     def _get_post_data(self, url, data):
         '''校務系統通過 post 匯出檔案的一般化邏輯'''
